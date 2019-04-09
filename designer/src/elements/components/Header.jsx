5ba7dbbd44eea546af25a66aea82cee17a0d2e75
nerd553 <geomelkon@csd.auth.gr>
2019-04-09 19:09:17 +0300

    Removed unecessary page title

--- designer/src/elements/components/Header.jsx
+++ designer/src/elements/components/Header.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import localizedText from '../../localization/LocalizedText';
-
 const Header = ({username, profilePic}) => (
   <header>
     <p id="logo">Card Sorter</p>
-    <h1>{localizedText.text.studies}</h1>
     <div className="profile">
       <p>{username}</p>
       <img src={profilePic} alt='Profile' />
