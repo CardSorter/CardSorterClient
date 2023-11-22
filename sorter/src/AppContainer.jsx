d41459df972027910c866b0e5e6249f2923fe5f6
pkyriakou23 <72734444+pkyriakou23@users.noreply.github.com>
2023-11-22 23:26:32 +0200

    Update AppContainer.jsx

--- sorter/src/AppContainer.jsx
+++ sorter/src/AppContainer.jsx
@@ -1,14 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable indent */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import { DragDropContextProvider } from 'react-dnd';
+import {DragDropContextProvider} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import App from './App';
 
 const AppContainer = () => {
-    return (
-        <DragDropContextProvider backend={HTML5Backend}>
-            <App />
-        </DragDropContextProvider>
-    );
-}
+  return (
+    <DragDropContextProvider backend={HTML5Backend}>
+      <App />
+    </DragDropContextProvider>
+  );
+};
 
 export default AppContainer;
