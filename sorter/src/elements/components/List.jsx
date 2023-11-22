915126a38a4ddcd7b689e7cb87d347297137eb2e
pkyriakou23 <72734444+pkyriakou23@users.noreply.github.com>
2023-11-22 23:34:39 +0200

    Update List.jsx

--- sorter/src/elements/components/List.jsx
+++ sorter/src/elements/components/List.jsx
@@ -1,21 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable new-cap */
+/* eslint-disable require-jsdoc */
 // eslint-disable-next-line no-unused-vars
-import { React, useEffect, useState } from 'react';
+import {React, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line no-unused-vars
 import CardItem from './CardItem.jsx';
-import { fetchCards } from '../../actions/cardAction.js';
+import {fetchCards} from '../../actions/cardAction.js';
 
-import { DropTarget } from 'react-dnd';
-import { itemTypes } from '../../staticContent/dragConstants';
+import {DropTarget} from 'react-dnd';
+import {itemTypes} from '../../staticContent/dragConstants';
 
 const listTarget = {
   drop(props, monitor) {
     const card = monitor.getItem();
     props.onDrop(card.id, card.position, props.categories);
-
-  }
-}
+  },
+};
 
 function collect(connect, monitor) {
   return {
@@ -24,7 +26,7 @@ function collect(connect, monitor) {
   };
 }
 
-const List = ({ cards, categories, onDrop, connectDropTarget }) => {
+const List = ({cards, categories, onDrop, connectDropTarget}) => {
   useEffect(() => {
     const handleBeforeUnload = (event) => {
       event.preventDefault();
@@ -37,15 +39,15 @@ const List = ({ cards, categories, onDrop, connectDropTarget }) => {
 
   cards = cards.cards;
   return connectDropTarget(
-    <ul id='list'>
-      {
-        cards.map((card) => (
-          <CardItem key={card.id} id={card.id} title={card.title}
-            description={card.description} position={-1}
-          />
-        ))
-      }
-    </ul>
+      <ul id='list'>
+        {
+          cards.map((card) => (
+            <CardItem key={card.id} id={card.id} title={card.title}
+              description={card.description} position={-1}
+            />
+          ))
+        }
+      </ul>,
   );
 };
 List.propTypes = {
